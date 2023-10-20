4d9a0e2c42a72824db3ad651fe7074ac145db984
lucaspari <lucas.pari@icloud.com>
2023-10-20 15:26:44 -0300

    feat: toggleterm,noice,remap,surround

--- lua/lucas/core/remap.lua
+++ lua/lucas/core/remap.lua
@@ -1,25 +1,26 @@
 vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>w", ":w<CR>")
-vim.keymap.set('n', '<leader>c', ':bdelete<CR>')
-vim.keymap.set('n', '<leader>q', ':q<CR>')
-vim.keymap.set('n', '<leader>vs', ':vsplit<CR>')
-vim.keymap.set('n', '<leader>sp', ':split<CR>')
-vim.keymap.set('n', '<leader>Q', ':q!<CR>')
-vim.keymap.set('v','<C-c>', '"+y')
+vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>c", ":bdelete<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>vs", ":vsplit<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>sp", ":split<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>Q", ":q!<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-Up>", ":horizontal resize +1<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-Up>", ":horizontal resize +1<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-Right>", ":vertical resize -1<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-Left>", ":vertical resize -1<CR>", { noremap = true })
+vim.keymap.set("v", "<C-c>", '"+y')
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
--- Function to move the current line up
--- Normal mode mappings
-vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
 
 -- Visual mode mappings
-vim.api.nvim_set_keymap('x', '<A-j>', ':m \'>+1<CR>gv=gv', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('x', '<A-k>', ':m \'<-2<CR>gv=gv', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap("x", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
--- lua/lucas/plugins/toggleterm.lua
+++ lua/lucas/plugins/toggleterm.lua
@@ -3,10 +3,6 @@ return {
 		"akinsho/toggleterm.nvim",
 		version = "*",
 		config = function()
-			local status_ok, toggleterm = pcall(require, "toggleterm")
-			if not status_ok then
-				return
-			end
 			require("toggleterm").setup({
 				size = 20,
 				open_mapping = [[<c-\>]],
@@ -29,10 +25,6 @@ return {
 					},
 				},
 			})
-			function _G.set_terminal_keymaps()
-				local opts = { silent = true, noremap = true }
-				vim.api.nvim_buf_set_keymap("n", "<C-\\>", ":ToggleTerm direction=float<CR>", opts)
-			end
 		end,
 	},
 }
