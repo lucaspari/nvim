7efb5fcf135072ccf94a3ff884bd738e6b44c207
lucaspari <lucas.pari@icloud.com>
2023-10-20 15:36:35 -0300

    feat: added git to telescope

--- lua/lucas/plugins/telescope.lua
+++ lua/lucas/plugins/telescope.lua
@@ -8,5 +8,6 @@ return {
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fa', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
     end
 }
