ccac7cd226f2c569868df6be05a9a8fc15d95926
lucaspari <lucas.pari@icloud.com>
2024-05-06 11:19:15 -0300

    feat: Update alpha-nvim plugin configuration
    This commit updates the configuration for the alpha-nvim plugin. It adds a new section to the dashboard header and modifies the buttons section. Additionally, it introduces a random footer phrase from a predefined list. This change aims to enhance the visual appearance and functionality of the alpha-nvim plugin.

--- lua/lucaspari/plugins/alpha-nvim.lua
+++ lua/lucaspari/plugins/alpha-nvim.lua
@@ -1,44 +1,52 @@
 return {
-  "goolord/alpha-nvim",
-  config = function()
-    local status_ok, alpha = pcall(require, "alpha")
-    if not status_ok then
-      return
-    end
+	"goolord/alpha-nvim",
+	config = function()
+		local phrases = {
+			"Hate is a place where a man who can't stand sadness goes",
+			"The reward for ambition too great… is self destruction.",
+			"Do, or do not. There is no “try”",
+			"Every man dies, but not every man really lives.",
+			"Great men are not born great, they grow great.",
+		}
+		local status_ok, alpha = pcall(require, "alpha")
+		if not status_ok then
+			return
+		end
 
-    local dashboard = require("alpha.themes.dashboard")
-    dashboard.section.header.val = {
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
-    }
+		local dashboard = require("alpha.themes.dashboard")
+		dashboard.section.header.val = {
+			"                                                     ",
+			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+			"                                                     ",
+		}
 
-    dashboard.section.buttons.val = {
-      dashboard.button("p", "  Find Project", ":Telescope projects <CR>"),
-      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("r", "󰎔  Recently used files", ":Telescope oldfiles <CR>"),
-      dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-      dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua<CR>"),
-      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
-    }
+		dashboard.section.buttons.val = {
+			dashboard.button("p", "  Find Project", ":Telescope projects <CR>"),
+			dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("r", "󰎔  Recently used files", ":Telescope oldfiles <CR>"),
+			dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+			dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua<CR>"),
+			dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+		}
 
-    local function footer()
-      return "Don't Stop Until You are Proud..."
-    end
+		local function footer()
+			local random_element = phrases[math.random(#phrases)]
+			return random_element
+		end
 
-    dashboard.section.footer.val = footer()
+		dashboard.section.footer.val = footer()
 
-    dashboard.section.footer.opts.hl = "Type"
-    dashboard.section.header.opts.hl = "Include"
-    dashboard.section.buttons.opts.hl = "Keyword"
+		dashboard.section.footer.opts.hl = "Type"
+		dashboard.section.header.opts.hl = "Include"
+		dashboard.section.buttons.opts.hl = "Keyword"
 
-    dashboard.opts.opts.noautocmd = true
-    alpha.setup(dashboard.opts)
-  end
+		dashboard.opts.opts.noautocmd = true
+		alpha.setup(dashboard.opts)
+	end,
 }
