722cf748f3978029c37919a6b9cd600d4a1d810c
lucaspari <lucas.pari@icloud.com>
2024-05-10 14:37:41 -0300

    feat: added telescope ui for code actions

--- lua/lucaspari/plugins/telescope.lua
+++ lua/lucaspari/plugins/telescope.lua
@@ -1,57 +1,68 @@
 -- plugins/telescope.lua:
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.6",
-	-- or                              , branch = '0.1.x',
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>ft", builtin.live_grep, {})
-		vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-		vim.keymap.set("n", "<leader>tt",builtin.current_buffer_fuzzy_find , {})
-		vim.keymap.set("n", "<leader>cmd",builtin.commands, {})
-		require("telescope").setup({
-			defaults = {
-				-- Default configuration for telescope goes here:
-				-- config_key = value,
-				mappings = {
-					i = {
-						-- map actions.which_key to <C-h> (default: <C-/>)
-						-- actions.which_key shows the mappings for your picker,
-						-- e.g. git_{create, delete, ...}_branch for the git_branches picker
-						["<C-j>"] = {
-							require("telescope.actions").move_selection_next,
-							type = "action",
-							opts = { nowait = true, silent = true },
-						},
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		-- or                              , branch = '0.1.x',
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>ft", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+			vim.keymap.set("n", "<leader>tt", builtin.current_buffer_fuzzy_find, {})
+			vim.keymap.set("n", "<leader>cmd", builtin.commands, {})
+			require("telescope").setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-j>"] = {
+								require("telescope.actions").move_selection_next,
+								type = "action",
+								opts = { nowait = true, silent = true },
+							},
 
-						["<C-k>"] = {
-							require("telescope.actions").move_selection_previous,
-							type = "action",
-							opts = { nowait = true, silent = true },
+							["<C-k>"] = {
+								require("telescope.actions").move_selection_previous,
+								type = "action",
+								opts = { nowait = true, silent = true },
+							},
 						},
 					},
 				},
-			},
-			pickers = {
-				-- Default configuration for builtin pickers goes here:
-				-- picker_name = {
-				--   picker_config_key = value,
-				--   ...
-				-- }
-				-- Now the picker_config_key will be applied every time you call this
-				-- builtin picker
-			},
-			extensions = {
-				-- Your extension configuration goes here:
-				-- extension_name = {
-				--   extension_config_key = value,
-				-- }
-				-- please take a look at the readme of the extension you want to configure
-			},
-		})
-	end,
+				pickers = {
+					-- Default configuration for builtin pickers goes here:
+					-- picker_name = {
+					--   picker_config_key = value,
+					--   ...
+					-- }
+					-- Now the picker_config_key will be applied every time you call this
+					-- builtin picker
+				},
+				extensions = {
+					-- Your extension configuration goes here:
+					-- extension_name = {
+					--   extension_config_key = value,
+					-- }
+					-- please take a look at the readme of the extension you want to configure
+				},
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			-- This is your opts table
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
