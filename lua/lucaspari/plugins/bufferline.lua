e2ff4acb2ae26c5739a87ece7b2d225a38dc045c
lucaspari <lucas.pari@icloud.com>
2024-05-24 12:00:50 -0300

    feat: added lazygit and changed bufferline theme

--- lua/lucaspari/plugins/bufferline.lua
+++ lua/lucaspari/plugins/bufferline.lua
@@ -7,11 +7,6 @@ return {
 			options = {
 				diagnostics = "nvim_lsp",
 				separator_style = "slant",
-				offsets = {
-					{
-						filetype = "NvimTree",
-					},
-				},
 			},
 			highlights = {
 				-- fill = { bg = "#24283b" }, -- Change the background color here
